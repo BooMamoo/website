cf8f03c60c616b368790b5aa3fc9a33efad54c26
BooMamoo <yanakorn_eve@hotmail.com>
2016-02-12 18:08:00 +0000

    Design 'NETPIEPage'

--- public/js/Controller/NetpieController.js
+++ public/js/Controller/NetpieController.js
@@ -1,3 +1,20 @@
 app.controller('NetpieController', function($scope, data) {
-	$scope.data = data.data[0].payload;
+	var tmp = data.data[0].payload;
+
+	tmp = tmp.replace(/data/g, '');
+	// $scope.data = $scope.data.replace(/%5B/g, '[');
+	// $scope.data = $scope.data.replace(/%5D/g, ']');
+	tmp = tmp.replace(/%5B/g, '');
+	tmp = tmp.replace(/%5D/g, '');
+	tmp = tmp.replace(/%20/g, ' ');
+	tmp = tmp.replace(/%3A/g, ':');
+
+	tmp = tmp.split('&');
+
+	$scope.data = [];
+
+	for(var i = 0 ; i < tmp.length ; i++)
+	{
+		$scope.data.push(tmp[i].split('='));
+	}
 });
